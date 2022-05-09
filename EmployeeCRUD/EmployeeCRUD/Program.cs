f455ac8fa581601c8cb34d00cc01f22715efd114
yenneralayon142 <yenner.alayon1@misena.edu.co>
2022-05-09 18:04:36 -0500

    Conciliando Cambios

--- EmployeeCRUD/EmployeeCRUD/Program.cs
+++ EmployeeCRUD/EmployeeCRUD/Program.cs
@@ -3,6 +3,13 @@ using Microsoft.EntityFrameworkCore;
 using EmployeeCRUD.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection");;
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));;
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();;
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -27,6 +34,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
